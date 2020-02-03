d269338258b4e1b077424f36fc35f0b17b2a130c
Chris Padilla <cdpadilla42@gmail.com>
2020-02-03 11:27:04 -0600

    Initial commit

---
+++ tests/ships.test.js
@@ -0,0 +1,9 @@
+test.todo('Ships facotry returns appropriate lengthed ship', () => {
+  expect(shipsFactory(4).length).toBe(4);
+});
+
+test.todo('Ship factory shows where a ship has been hit');
+
+test.todo('Ship factory shows if sunk');
+
+test.todo('ship factory shows if afloat');
