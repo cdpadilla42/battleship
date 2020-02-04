b90291c1c0da2f29581b32287f0290ed7450c525
Chris Padilla <cdpadilla42@gmail.com>
2020-02-04 09:33:58 -0600

    add ship overflow rejection functionality

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,14 +3,47 @@ import shipsFactory from './ships';
 const gameboard = function() {
   let model = new Array(64).fill(null);
 
+  const isShipOverflowing = (shipLength, location) => {
+    let edge;
+    if (location < 8) {
+      edge = 8;
+    } else if (location >= 8 && location < 16) {
+      edge = 16;
+    } else if (location >= 16 && location < 24) {
+      edge = 24;
+    } else if (location >= 24 && location < 32) {
+      edge = 32;
+    } else if (location >= 32 && location < 40) {
+      edge = 40;
+    } else if (location >= 40 && location < 48) {
+      edge = 48;
+    } else if (location >= 48 && location < 56) {
+      edge = 56;
+    } else {
+      edge = 64;
+    }
+
+    if (location + shipLength >= edge) {
+      return true;
+    }
+    return false;
+  };
+
   const placeShip = function(ship, location) {
     const shipName = ship.showName();
     const shipLength = ship.showLength();
+    if (isShipOverflowing(shipLength, location)) {
+      return placementError();
+    }
     let counter = 1;
-    for (var i = location; i < shipLength; i++) {
+    for (var i = location; i < shipLength + location; i++) {
       model[i] = `${shipName}${counter}`;
       counter++;
     }
+  };
+
+  const placementError = () => {
+    return new Error('Could not place ships');
   };
 
   const showBoard = () => model;
@@ -21,7 +54,7 @@ const gameboard = function() {
   // first corner is selected by player, then proceeding parts of array are filled in
   // hit represented with an X. Blank hit with a 'miss'
 
-  return { placeShip, showBoard };
+  return { placeShip, showBoard, placementError };
 };
 
 export { gameboard as default };
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -10,3 +10,134 @@ test('Places a single ship on the board', () => {
   expected[0] = 'A1';
   expect(currentBoard.showBoard()).toStrictEqual(expected);
 });
+
+test('Places a ship of length 3 on the board', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(3, 'A');
+  currentBoard.placeShip(shipA, 0);
+
+  let expected = new Array(64).fill(null);
+  expected[0] = 'A1';
+  expected[1] = 'A2';
+  expected[2] = 'A3';
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('Places a ship of length 1 on the board NOT at the start', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(1, 'A');
+  currentBoard.placeShip(shipA, 4);
+
+  let expected = new Array(64).fill(null);
+  expected[4] = 'A1';
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('Places a ship of length 1 on the board NOT at the start', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(1, 'A');
+  currentBoard.placeShip(shipA, 10);
+
+  let expected = new Array(64).fill(null);
+  expected[10] = 'A1';
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+// Ships conform to grid layout - 8 x 8
+// Breaking points: 8, 16, 24, 32, 40, 48, 56, 64
+
+test('board rejects a ship that overflows the board', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 7)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('board rejects a ship that overflows the board 2', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 15)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('board rejects a ship that overflows the board 3', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 23)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('board rejects a ship that overflows the board 4', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 31)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('board rejects a ship that overflows the board 5', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 39)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('board rejects a ship that overflows the board 6', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 47)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('board rejects a ship that overflows the board 7', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 55)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
+
+test('board rejects a ship that overflows the board 8', () => {
+  const currentBoard = gameboard();
+  const shipA = shipsFactory(2, 'A');
+  expect(currentBoard.placeShip(shipA, 63)).toStrictEqual(
+    currentBoard.placementError()
+  );
+
+  // expect to return error and leave board unchanged
+  let expected = new Array(64).fill(null);
+  expect(currentBoard.showBoard()).toStrictEqual(expected);
+});
