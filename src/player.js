945eb3796a8e7c9d0a3a35d4608254c9061426e7
Chris Padilla <cdpadilla42@gmail.com>
2020-02-05 10:19:02 -0600

    Add player and computer factories

--- src/player.js
+++ src/player.js
@@ -2,15 +2,32 @@ import gameboard from './gameboard';
 
 const playerFactory = function() {
   const playerBoard = gameboard();
-  const move = coord => {};
+  const move = (target, coord) => {
+    target.playerBoard.recieveAttack(coord);
+  };
   const showPlayerBoard = () => {
     return playerBoard.showBoard();
   };
-  return { showPlayerBoard, move };
+  return { move, playerBoard };
 };
 
 const computer = function() {
   const playerBoard = gameboard();
+
+  const move = (target, coord) => {
+    target.playerBoard.recieveAttack(coord);
+  };
+
+  const getRandomInt = function(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+
+  const randomMove = target => {
+    const randomCoord = getRandomInt(64);
+    move(target, randomCoord);
+  };
+
+  return { randomMove, playerBoard };
 };
 
-export { playerFactory as default };
+export { playerFactory, computer };
--- tests/player.test.js
+++ tests/player.test.js
@@ -1,17 +1,34 @@
-import playerFactory from '../src/player';
+import { playerFactory, computer } from '../src/player';
 
 describe('Player moves', () => {
   const playerOne = playerFactory();
   const playerTwo = playerFactory();
 
-  // HOLD UP!!! REFACTOR ME
-  // So that the player class is dependent on the board
-  // NOT the other way around! OR make the gameboard accessible to the player,
-  // but it won't be accessible to anything OUTSIDE the player function
-  // come back at this with a fresh mind, baby
+  const currentBoard = playerTwo.playerBoard;
+  const shipsObj = currentBoard.showShips();
+  currentBoard.placeShip(shipsObj.A, 0);
+  currentBoard.placeShip(shipsObj.B, 3);
+  currentBoard.placeShip(shipsObj.C, 7);
 
   test('move initiates an attack on other player board', () => {
-    playerOne.move(0);
+    playerOne.move(playerTwo, 0);
+    const arrFill = new Array(56).fill(null);
+    const expected = [
+      'X',
+      'A2',
+      null,
+      'B1',
+      'B2',
+      'B3',
+      null,
+      'C1',
+      ...arrFill
+    ];
+    expect(playerTwo.playerBoard.showBoard()).toStrictEqual(expected);
+  });
+
+  test('move initiates an attack on other player board 2', () => {
+    playerOne.move(playerTwo, 2);
     const arrFill = new Array(56).fill(null);
     const expected = [
       'X',
@@ -24,7 +41,7 @@ describe('Player moves', () => {
       'C1',
       ...arrFill
     ];
-    expect(playerTwo.showPlayerBoard()).toStrictEqual(expected);
+    expect(playerTwo.playerBoard.showBoard()).toStrictEqual(expected);
   });
 });
 
@@ -32,5 +49,26 @@ describe('Computer moves', () => {
   const playerOne = playerFactory();
   const computerPlayer = computer();
 
-  test.toDo('computer move randomly places move on other board');
+  const currentBoard = playerOne.playerBoard;
+  const shipsObj = currentBoard.showShips();
+  currentBoard.placeShip(shipsObj.A, 0);
+  currentBoard.placeShip(shipsObj.B, 3);
+  currentBoard.placeShip(shipsObj.C, 7);
+
+  test('computer move randomly places move on other board', () => {
+    computerPlayer.randomMove(playerOne);
+    const arrFill = new Array(56).fill(null);
+    const expected = [
+      'A1',
+      'A2',
+      null,
+      'B1',
+      'B2',
+      'B3',
+      null,
+      'C1',
+      ...arrFill
+    ];
+    expect(playerOne.playerBoard.showBoard()).not.toStrictEqual(expected);
+  });
 });
